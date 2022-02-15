dcdd7a2adc73d705136525620ad5a2c3718574f1
João Oliveira <novojoaojm@gmail.com>
2022-02-15 10:55:36 -0300

    add cors methods

--- src/app.js
+++ src/app.js
@@ -11,9 +11,14 @@ app.use(express.json())
 
 // routes
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, *');
+  if (req.method === 'OPTIONS'){
+      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      return res.status(200).json({});
+  }
   next();
 });
 
