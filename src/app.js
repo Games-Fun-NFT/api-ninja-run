99bdeffca0cc978c42a773254095743dd1ae9384
João Oliveira <novojoaojm@gmail.com>
2022-02-15 07:33:40 -0300

    add cors methods

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ const cookieParser = require('cookie-parser');
 const cors    = require('cors')
 const jwt     = require('jsonwebtoken')
 
-app.use(cors())
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}))
 app.use(cookieParser())
 app.set('trust proxy', true)
 app.use(express.json())
